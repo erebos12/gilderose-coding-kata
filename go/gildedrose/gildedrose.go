558d06d8df0732abd6dd2e097b820291ab7650ac
Alex Sahm <alexander.sahm@aleri.de>
2023-12-07 10:16:03 +0100

    more refactoring

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -5,6 +5,72 @@ type Item struct {
 	SellIn, Quality int
 }
 
+const (
+	MAX_QUALITY    = 50
+	SULFURAS       = "Sulfuras, Hand of Ragnaros"
+	AGED_BRIE      = "Aged Brie"
+	BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+	CONJURED       = "Conjured Mana Cake"
+)
+
+var EXCEPTIONAL_ITEMS = []string{BACKSTAGE_PASS, AGED_BRIE, SULFURAS}
+
+func UpdateQuality(items []*Item) {
+	for _, item := range items {
+		if item.Name != SULFURAS {
+			item.SellIn--
+		}
+		updateItemQuality(item)
+		if item.SellIn < 0 {
+			handleExpiredItem(item)
+		}
+	}
+}
+
+func updateItemQuality(item *Item) {
+	if containsString(EXCEPTIONAL_ITEMS, item.Name) {
+		switch item.Name {
+		case BACKSTAGE_PASS:
+			handleQualityForBackStagePass(item)
+		default:
+			if item.Name != SULFURAS {
+				item.IncreaseQualityBy(1)
+			}
+		}
+	} else {
+		decreaseValue := 1
+		if item.Name == CONJURED {
+			decreaseValue = 2
+		}
+		item.DecreaseQualityBy(decreaseValue)
+	}
+}
+
+func handleExpiredItem(item *Item) {
+	if !containsString(EXCEPTIONAL_ITEMS, item.Name) {
+		decreaseValue := 1
+		if item.Name == CONJURED {
+			decreaseValue = 2
+		}
+		item.DecreaseQualityBy(decreaseValue)
+	} else if item.Name == BACKSTAGE_PASS {
+		item.Quality = 0
+	}
+}
+
+func handleQualityForBackStagePass(item *Item) {
+	switch {
+	case item.SellIn < 0:
+		item.Quality = 0
+	case item.SellIn <= 5:
+		item.IncreaseQualityBy(3)
+	case item.SellIn <= 10:
+		item.IncreaseQualityBy(2)
+	default:
+		item.IncreaseQualityBy(1)
+	}
+}
+
 func (item *Item) DecreaseQualityBy(decreaseValue int) {
 	if decreaseValue > item.Quality {
 		item.Quality = 0
@@ -22,46 +88,6 @@ func (item *Item) IncreaseQualityBy(increaseValue int) {
 	}
 }
 
-var MAX_QUALITY = 50
-
-var SULFURAS = "Sulfuras, Hand of Ragnaros"
-var AGED_BRIE = "Aged Brie"
-var BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
-var CONJURED = "Conjured Mana Cake"
-var EXCEPTIONAL_ITEMS = []string{BACKSTAGE_PASS, AGED_BRIE, SULFURAS}
-
-func UpdateQuality(items []*Item) {
-	for _, item := range items {
-		if item.Name != SULFURAS {
-			item.SellIn = item.SellIn - 1
-		}
-		if !containsString(EXCEPTIONAL_ITEMS, item.Name) {
-			if item.Name == CONJURED {
-				item.DecreaseQualityBy(2)
-			} else {
-				item.DecreaseQualityBy(1)
-			}
-		} else {
-			if item.Name == BACKSTAGE_PASS {
-				handleQualityForBackStagePass(item)
-			} else {
-				if item.Name != SULFURAS {
-					item.IncreaseQualityBy(1)
-				}
-			}
-		}
-		if item.SellIn < 0 {
-			if !containsString(EXCEPTIONAL_ITEMS, item.Name) {
-				if item.Name == CONJURED {
-					item.DecreaseQualityBy(2)
-				} else {
-					item.DecreaseQualityBy(1)
-				}
-			}
-		}
-	}
-}
-
 func containsString(slice []string, str string) bool {
 	for _, item := range slice {
 		if item == str {
@@ -69,17 +95,4 @@ func containsString(slice []string, str string) bool {
 		}
 	}
 	return false
-}
-
-func handleQualityForBackStagePass(item *Item) {
-	switch {
-	case item.SellIn < 0:
-		item.Quality = 0
-	case item.SellIn <= 5:
-		item.IncreaseQualityBy(3)
-	case item.SellIn <= 10:
-		item.IncreaseQualityBy(2)
-	default:
-		item.IncreaseQualityBy(1)
-	}
 }
