375b1ce7245b62ae5958e350ed4ffb57a4bf2708
Alex Sahm <alexander.sahm@aleri.de>
2023-12-12 10:06:30 +0100

    use table-driven testing

--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -1,199 +1,47 @@
 package gildedrose_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNormalItemQualityDecrease(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: "foo", SellIn: 10, Quality: 10},
+func TestUpdateQuality(t *testing.T) {
+	testCases := []struct {
+		tc, itemName                    string
+		sellIn, quality                 int
+		expectedSellIn, expectedQuality int
+	}{
+		{"NormalItemQualityDecrease", "item X", 10, 10, 9, 9},
+		{"NormalItemReachesZeroQuality", "item Z", 10, 1, 9, 0},
+		{"NormalItemReachesZeroQuality", gildedrose.SULFURAS, 0, 30, 0, 30},
+		{"AgedBrieGetsBetterWithAge", gildedrose.AGED_BRIE, 10, 40, 9, 41},
+		{"QualityNeverDropsBelowZero", "item A", 10, 0, 9, 0},
+		{"QualityNeverExceedsFifty", gildedrose.AGED_BRIE, 10, 50, 9, 50},
+		{"BackstageQualityWithNormalSellIn", gildedrose.BACKSTAGE_PASS, 15, 20, 14, 21},
+		{"BackstagePassQualityWithSellInLessThan10Days", gildedrose.BACKSTAGE_PASS, 9, 3, 8, 5},
+		{"BackstagePassQualityWithSellInEqual10Days", gildedrose.BACKSTAGE_PASS, 10, 7, 9, 9},
+		{"BackstagePassQualityWithSellInLessThan5Days", gildedrose.BACKSTAGE_PASS, 4, 3, 3, 6},
+		{"BackstagePassQualityWithSellInEqual5Days", gildedrose.BACKSTAGE_PASS, 5, 3, 4, 6},
+		{"BackstagePassQualityDropsToZeroAfterConcert", gildedrose.BACKSTAGE_PASS, -1, 3, -2, 0},
+		{"ExpiredItemReducesQualityTwiceAsFast", "item B", -1, 5, -2, 3},
+		{"DecreaseQualityTwiceAsFastForConjuredItem", gildedrose.CONJURED, 10, 12, 9, 10},
+		{"QualityReductionDoublesForConjuredItemAfterSellIn", gildedrose.CONJURED, -2, 12, -3, 8},
 	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 9
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 9
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
 
-}
-
-func TestNormalItemReachesZeroQuality(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: "foo", SellIn: 10, Quality: 1},
+	for _, test := range testCases {
+		t.Run(test.tc, func(t *testing.T) {
+			items := []*gildedrose.Item{
+				{Name: test.itemName, SellIn: test.sellIn, Quality: test.quality},
+			}
+			gildedrose.UpdateQuality(items)
+			assert.Equal(t, test.expectedQuality, items[0].Quality, "Wrong value for quality")
+			assert.Equal(t, test.expectedSellIn, items[0].SellIn, "Wrong value for sellIn")
+		})
 	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 0
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 9
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestSulfurasStaysLegendary(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.SULFURAS, SellIn: 0, Quality: 30},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 30
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 0
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestAgedBrieGetsBetterWithAge(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.AGED_BRIE, SellIn: 10, Quality: 40},
-		{Name: gildedrose.AGED_BRIE, SellIn: 9, Quality: 41},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 41
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 9
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-
-	expectedQuality = 42
-	assert.Equal(t, expectedQuality, items[1].Quality)
-	expectedSellIn = 8
-	assert.Equal(t, expectedSellIn, items[1].SellIn)
-}
-
-func TestQualityNeverDropsBelowZero(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: "Some item", SellIn: 10, Quality: 0},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 0
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 9
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestBackstageQualityWithNormalSellIn(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.BACKSTAGE_PASS, SellIn: 15, Quality: 20},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 21
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 14
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestBackstagePassQualityWithSellInLessThan10Days(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.BACKSTAGE_PASS, SellIn: 9, Quality: 3},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 5
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 8
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestBackstagePassQualityWithSellInLessThan5Days(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.BACKSTAGE_PASS, SellIn: 4, Quality: 3},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 6
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 3
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestBackstagePassQualityDropsToZeroAfterConcert(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.BACKSTAGE_PASS, SellIn: -1, Quality: 3},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 0
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := -2
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestExpiredItemReducesQualityTwiceAsFast(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: "Some item", SellIn: -1, Quality: 5},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 3
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := -2
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestDecreaseQualityTwiceAsFastForConjuredItem(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.CONJURED, SellIn: 10, Quality: 12},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 10
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 9
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestQualityReductionDoublesForConjuredItemAfterSellIn(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.CONJURED, SellIn: -2, Quality: 12},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 8
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := -3
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
-}
-
-func TestQualityNeverExceedsFifty(t *testing.T) {
-	// given
-	var items = []*gildedrose.Item{
-		{Name: gildedrose.AGED_BRIE, SellIn: 10, Quality: 50},
-	}
-	// when
-	gildedrose.UpdateQuality(items)
-	// then
-	expectedQuality := 50
-	assert.Equal(t, expectedQuality, items[0].Quality)
-	expectedSellIn := 9
-	assert.Equal(t, expectedSellIn, items[0].SellIn)
+	fmt.Printf("Executed '%d' testcases in total", len(testCases))
 }
 
 func Test_Fixture(t *testing.T) {
